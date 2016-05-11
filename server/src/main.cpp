3135ad941da199f7a11fda37f29f49c2b9393c74
authext <wolf.srb.ns@hotmail.com>
2016-05-11 16:23:45 +0200

    Add some skeleton code to server

--- server/src/main.cpp
+++ server/src/main.cpp
@@ -2,8 +2,21 @@
 #include <pcap.h>
 #include "select_device.h"
 #include "thread.h"
+#include "mutex.h"
 
 using namespace std;
+
+
+mutex m;
+
+
+void* worker(void *handle)
+{
+  m.lock();
+  cout << "Hello " << endl;
+  m.unlock();
+  return nullptr;
+}
 
 
 int main(int argc, char *argv[])
@@ -15,4 +28,10 @@ int main(int argc, char *argv[])
 
 	pcap_t *handle1 = select_device(devs);
 	pcap_t *handle2 = select_device(devs);
+
+  thread th1(worker, (void*)handle1);
+  thread th2(worker, (void*)handle2);
+
+  th1.join();
+  th2.join();
 }
